d24eb4fbc5e1e3b24ba7786dcbb7e1652d2929ef
Alex Moreno Palacio <amoreno@preference.es>
2022-03-01 12:30:42 +0100

    Final provisional (falta css  e id automatico)

---
+++ EjercicioPracticas1/Models/Metadata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EjercicioPracticas1.Models
+{
+    public class UsuariosMetadata
+    {
+        [Display(Name = "Nombre")]
+        public string Nombre;
+
+        [Display(Name = "Apellidos")]
+        public string Apellidos;
+
+        [Display(Name = "Fecha de nacimiento")]
+        public string FechaNacimiento;
+
+        [Display(Name = "Genero")]
+        public string Genero;
+
+        [Display(Name = "Activo")]
+        public string Activo;
+    }
+    public class DocVentasMetadata
+    {
+        [Display(Name = "Estado del pedido")]
+        public string EstadoPedido;
+
+        [Display(Name = "Descripcion del pedido")]
+        public string DescPedido;
+
+    }
+}
---
+++ EjercicioPracticas1/Models/PartialClasses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EjercicioPracticas1.Models
+{
+    [MetadataType(typeof(UsuariosMetadata))]
+    public partial class Usuarios
+    {
+    }
+
+    [MetadataType(typeof(DocVentasMetadata))]
+    public partial class DocVentas
+    {
+    }
+}
